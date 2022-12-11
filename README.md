5dd5820baa78c1af57666257ece0358c908b0a6c
Tryliom <alexishaldy@gmail.com>
2022-12-11 22:38:16 +0100

    Add serenity example

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # proto-rust
+
+
+# How to install rust
+
+Go on [rust website](https://www.rust-lang.org/learn/get-started) and follow the instructions.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,66 @@
-fn main() {
-    println!("Hello, world!");
+mod commands;
+
+extern crate dotenv_codegen;
+
+use dotenv_codegen::dotenv;
+
+use serenity::async_trait;
+use serenity::model::application::command::Command;
+use serenity::model::application::interaction::{Interaction, InteractionResponseType};
+use serenity::model::gateway::Ready;
+use serenity::prelude::*;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn ready(&self, ctx: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+
+        Command::create_global_application_command(&ctx.http, |command| {
+            commands::test::register(command)
+        }).await.expect("Create command failed");
+    }
+
+    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
+        if let Interaction::ApplicationCommand(command) = interaction {
+            //println!("Received command interaction: {:#?}", command);
+
+            let content = match command.data.name.as_str() {
+                "test" => commands::test::run(&command.data.options),
+                _ => "not implemented :(".to_string(),
+            };
+
+            if let Err(why) = command
+                .create_interaction_response(&ctx.http, |response| {
+                    response
+                        .kind(InteractionResponseType::ChannelMessageWithSource)
+                        .interaction_response_data(|message| message.content(content))
+                })
+                .await
+            {
+                println!("Cannot respond to slash command: {}", why);
+            }
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    // Configure the client with your Discord bot token in the environment.
+    let token = dotenv!("DISCORD_TOKEN");
+
+    // Build our client.
+    let mut client = Client::builder(token, GatewayIntents::empty())
+        .event_handler(Handler)
+        .await
+        .expect("Error creating client");
+
+    // Finally, start a single shard, and start listening to events.
+    //
+    // Shards will automatically attempt to reconnect, and will perform
+    // exponential backoff until it reconnects.
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
+    }
 }
