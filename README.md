07a5fe77d40e53ba866a9ea698b162e3e002210e
Alexis Haldy <alexishaldy@gmail.com>
2022-12-14 16:44:02 +0100

    Try to use a gif library

--- README.md
+++ README.md
@@ -5,10 +5,6 @@
 
 Go on [rust website](https://www.rust-lang.org/learn/get-started) and follow the instructions.
 
-# How to install Magick-rust
-
-Go on [Magick-rust](https://github.com/nlfiedler/magick-rust) and follow the instructions.
-
 # Run configuration
 
 * Set the channel to `nightly` (you need to do this command on windows cmd `rustup toolchain install nightly`)
--- src/commands/test.rs
+++ src/commands/test.rs
@@ -1,4 +1,10 @@
-
+use std::borrow::Cow;
+use image::{Frame, Rgb, RgbImage};
+use image::codecs::gif;
+use image::ImageFormat::Gif;
+use imageproc::drawing;
+use imageproc::rect::Rect;
+use ::gif::{Encoder};
 
 use serenity::builder::CreateApplicationCommand;
 use serenity::model::application::interaction::{InteractionResponseType};
@@ -7,14 +13,31 @@ use serenity::model::prelude::interaction::application_command::{ApplicationComm
 use serenity::prelude::*;
 
 pub async fn run(command: &ApplicationCommandInteraction, ctx: &Context) -> serenity::Result<()> {
-    // Create a gif
 
+    let mut buffer = Vec::new();
+    let mut encoder = Encoder::new(&mut buffer, 100, 100, &[]).unwrap();
 
+    for i in 0..10 {
+        let mut frame = Frame::default();
+        frame.width = 100;
+        frame.height = 100;
+
+        let mut image = RgbImage::new(100, 100);
+        drawing::draw_filled_circle_mut(&mut image, (50, 50), 50, Rgb([i * 25, 0, 0]));
+
+        frame.buffer = Cow::from(image.to_vec());
+        frame.delay = 100;
+        encoder.write_frame(&frame).unwrap();
+    }
+
+    //TODO: Convert Vec to Attachment
+
+    // Send the image as a response
     command
         .create_interaction_response(&ctx.http, |response| {
             response
                 .kind(InteractionResponseType::ChannelMessageWithSource)
-                .interaction_response_data(|message| message.content("tutu"))
+                .interaction_response_data(|message| message.content("tutu") | message.add_file(&mut buffer))
         })
         .await
 }
